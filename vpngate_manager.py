ffbf692f979803fc5e77e20870ccb5545f9deeab
Iwithyou2025 <iwithyou2025@gmail.com>
2026-05-31 17:09:10 +0800

    again order country2

--- vpngate_manager.py
+++ vpngate_manager.py
@@ -46,7 +46,7 @@ MAX_SCAN_ROWS = int(os.environ.get("MAX_SCAN_ROWS", "300"))
 
 PREFERRED_COUNTRIES = [
     c.strip().upper()
-    for c in os.environ.get("PREFERRED_COUNTRIES", "KR,JP,US,CA").split(",")
+    for c in os.environ.get("PREFERRED_COUNTRIES", "JP,KR,US,CA").split(",")
     if c.strip()
 ]
 
