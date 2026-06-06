822ea34d719a200239c92e4b1d65c96909296cc9
Iwithyou2025 <iwithyou2025@gmail.com>
2026-06-06 13:55:59 +0800

    adjust page  layout bug..

--- vpngate_manager.py
+++ vpngate_manager.py
@@ -5069,9 +5069,7 @@ INDEX_HTML = r"""<!doctype html>
             刷新数据
           </button>
     
-          <div class="switch-chart-total">
-            已记录：<strong id="switch_duration_count">0</strong> 次
-          </div>
+ 
         </div>
       </div>
     
